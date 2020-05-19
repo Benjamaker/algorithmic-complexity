f56e03500c7e1844728693a7d45c919bb11cae85
Ben Galley <bengalley21@gmail.com>
2020-05-19 10:59:26 +0100

    added third test for reversing an array of 2 elements and passed test

--- spec/my_reverse_spec.rb
+++ spec/my_reverse_spec.rb
@@ -8,4 +8,8 @@ describe "my_reverse" do
   it("returns [1] for an array of [1]") do
     expect(my_reverse([1])).to eq([1])
   end  
+
+  it("returns [2, 1] for an array of [1, 2]") do
+    expect(my_reverse([1, 2])).to eq([2, 1])
+  end  
 end    
